a2f5c266164af7a9151bf097fed183d61a291f2a
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2025-03-08 01:51:38 +0530

    prisoner dashboard design 1

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,6 +1,11 @@
-
 import { Link } from "react-router-dom";
-import { FaRobot, FaBell } from "react-icons/fa"; // Importing the chatbot and bell icons from react-icons
+import {
+  FaRobot,
+  FaBell,
+  FaFileAlt,
+  FaGavel,
+  FaCalendarAlt,
+} from "react-icons/fa"; // Importing icons from react-icons
 import "../assets/css/dashboard.css"; // Importing the CSS file for styling
 
 const Dashboard = () => {
@@ -14,25 +19,19 @@ const Dashboard = () => {
       </header>
       <div className="dashboard-sections">
         <div className="dashboard-section document-management">
-          <h2>Easy Document Management</h2>
-          <hr />
-          <p>Manage and upload your documents easily.</p>
+          <FaFileAlt size={50} color="#F5A623" />
           <Link to="/doc-manager" className="dashboard-link">
             Go to Document Management
           </Link>
         </div>
         <div className="dashboard-section legal-aid-request">
-          <h2>Submit your cases</h2>
-          <hr />
-          <p>Simple and fast case registration</p>
+          <FaGavel size={50} color="#F5A623" />
           <Link to="/cases" className="dashboard-link">
-            Go to Case Form Submission
+            Submit your cases
           </Link>
         </div>
         <div className="dashboard-section schedule-management">
-          <h2>Working Calendar</h2>
-          <hr />
-          <p>Manage your court schedule and hearings.</p>
+          <FaCalendarAlt size={50} color="#F5A623" />
           <Link to="/calendar" className="dashboard-link">
             Go to Schedule Management
           </Link>
